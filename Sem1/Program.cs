808ca3ae39df6031bb86d65b6f5b96a83dbe73e3
Alexander Glotov <glob.nsk@mail.ru>
2024-02-19 15:47:54 +0300

    Add Homework

---
+++ Sem1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму первой и последней цифры трехзначного числа
+
+int num = 666;
+int first = num / 100;
+int second = num%10;
+int sum = first + second;
+Console.Write("sum of last &second ="+sum);
+
