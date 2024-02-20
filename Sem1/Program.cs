56617cd556efb411113e759faccf9d17f8134de3
Alexander Glotov <glob.nsk@mail.ru>
2024-02-20 10:10:33 +0300

    Add Task4 in Sem1/Task1 in Sem2

--- Sem1/Program.cs
+++ Sem1/Program.cs
@@ -1,8 +1,52 @@
-﻿// Найти сумму первой и последней цифры трехзначного числа
+﻿// Задача 4
 
-int num = 666;
-int first = num / 100;
-int second = num%10;
-int sum = first + second;
-Console.Write("sum of last &second ="+sum);
+// int N = 27;
+// int num = 0;
 
+// while (num<=N)
+// {
+    //Console.WriteLine (num);
+    //num=num+2;
+// }
+
+// Задача 1
+//int a = 256;
+//int b = a%10;
+//int d = (a / 100) * 10 + a%10;
+//Console.WriteLine(d);
+
+// Задача 2
+
+//int a = 617;
+//int c = ((a%100) / 10);
+//int d = a%10;
+//double b = Math.Pow(c,d);
+//Console.WriteLine(b);
+
+//Задача 3
+//int a = 1024;
+//int b = 12;
+//if (a % b == 0)
+//{
+  //  Console.Write("Да");
+//}
+//else 
+//{
+   // Console.Write("НЕТ," +' '+(a % b));  
+//}
+
+
+//Задача 4
+
+int a = 6;
+int b = a / 100;
+int c = (b / 10)*10;
+int d = b - c;
+if (b == 0)
+{
+Console.Write("Третьей цифры нет");
+}
+else 
+{
+    Console.Write(d);
+}
---
+++ Sem2/Program.cs
@@ -0,0 +1,14 @@
+﻿//ЗАДАЧА 1
+
+int num = 14;
+if (num % 7 == 0)
+{ 
+    if (num % 23 == 0)
+    {
+       Console.Write("ДА");
+    }
+    else
+    Console.Write("НЕТ");
+    }
+    else
+Console.Write("НЕТ");
