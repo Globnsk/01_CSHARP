d5016bcb0c1f0ce00c9c4e8f294706abcfba4237
Globnsk <glob.nsk@mail.ru>
2024-02-25 22:50:42 +0300

    Add task3

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,11 +1,20 @@
 ﻿int N = 10;
 int[] arr = new int[N];
 int i = 0;
-while (i<N)
+while (i < N)
 {
-arr[i] = i + 1;
-Console.Write (arr[i] + ",");
-i = i + 1;
+    arr[i] = i + 1;
+    // Console.Write(arr[i] + ",");
+    i = i + 1;
 }
 
+i = 0;
+while (i < N)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr [i]},");
 
+    }
+    i = i + 1;
+}
