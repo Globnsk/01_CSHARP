2b990d0f099f4a92d92c839e3d99ab2c1592a7b8
Alexander Glotov <glob.nsk@mail.ru>
2024-02-26 15:51:54 +0300

    Add Less3 task1 Functions

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denuminator = c + d;
+    double result = numinator / denuminator;
+    return result;
+}
+double answer = CalculateFormula(1, 2, 3, 4);
+Console.Write(answer);
