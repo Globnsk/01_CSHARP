38f74b34c5958c06af9a40cdbf0bb53d0acd072e
Alexander Glotov <glob.nsk@mail.ru>
2024-03-12 11:29:31 +0300

    Add Sem6

---
+++ Sem6 Homework/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 3: Проверка строки на палиндром
+
+// Входная строка для проверки
+Console.WriteLine("Введите сроку символов");
+string input = Console.ReadLine();
+// Вызов метода для проверки, является ли строка палиндромом
+bool isPalindrome = IsPalindrome(input);
+// Вывод результата
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+
+// Метод для проверки, является ли строка палиндромом
+bool IsPalindrome(string str)
+{
+    // Нормализация строки путем удаления не буквенно-цифровых
+    //символов и приведения к нижнему регистру
+    string normalized = new
+    string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    // Сравнение строки с ее перевернутым вариантом
+    return normalized.SequenceEqual(normalized.Reverse());
+}
+
---
+++ Sem6 Homework/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4 : Обратный порядок слов в строке
+
+// Входная строка со словами, разделенными пробелами
+string input = Console.ReadLine();
+// Вызов метода для обращения порядка слов в строке
+string result = ReverseWords(input);
+// Вывод результата
+Console.WriteLine(result);
+
+// Метод для обращения порядка слов в строке
+string ReverseWords(string str)
+{
+    // Разделение строки на слова
+    string[] words = str.Split(' ');
+    // Обращение порядка слов
+    Array.Reverse(words);
+    // Соединение слов обратно в строку с пробелами
+    return string.Join(" ", words);
+}
+
